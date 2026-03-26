b32c5fafa16894f701f7287716d3f28f9c44d077
arthurgxo <arthurgxo2@gmail.com>
2026-03-26 11:54:28 -0300

    Criado esqueleto para suporte de ingles

--- lerolero.py
+++ lerolero.py
@@ -10,5 +10,11 @@ parte1 = []
 parte2 = []
 paret3 = []
 
+lingua = int(input("Escolha a lingua: \n 1: Portugues\n 2: Ingles\n"))
+
+if lingua == 2 :
+    parte1 = []
+    parte2 = []
+    parte3 = []
 
 print(random.choice(parte1), random.choice(parte1), random.choice(parte1))
