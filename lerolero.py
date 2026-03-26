1bffd005ebb21adb7da3e636ee73aa1df8a57972
arthurgxo <arthurgxo2@gmail.com>
2026-03-26 11:40:46 -0300

    criada estrutura principal do programa

--- lerolero.py
+++ lerolero.py
@@ -1,7 +1,14 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 
 # Gerador de LeroLero
 # Gera texto sem sentido
 
 
+import random
 
+parte1 = []
+parte2 = []
+paret3 = []
+
+
+print(random.choice(parte1), random.choice(parte1), random.choice(parte1))
